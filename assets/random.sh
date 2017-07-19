a74ed5f4dab6aa872c9ca5d42b1cf7b7e27c793b
InnovativeInventor <InnovativeInventor@gmail.com>
2017-07-19 09:45:05 -0400

    Some changes

---
+++ assets/random.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+echo "Making sudo, please ensure that you have verified this script's checksum"
+sudo -v
+SIZE=$((100))
+while getopts d:s:a option
+do
+	case "${option}"
+	in
+	d) DOCKER=${OPTARG};;
+	s) SIZE=${OPTARG};;
+	a) APP=${OPTARG};;
+	esac
+done
+
+echo "Running random.sh, with options -d $DOCKER -s $SIZE -a $APP at date +"%c"" >> /etc/rnd/random.log
+
+# OS-detection
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	apt-get install -y haveged
+	echo "Detected GNU/Linux, installing haveged" >> /etc/rnd/random.log
+fi
+
+# might be useful: LC_ALL=C sed s/\"//
+mkdir -p /etc/rnd
+chmod -R 775 /etc/rnd
+chmod 775 /etc/rnd/random
+chmod 775 /etc/rnd/random_openssl
+
+# Generating random numbers a few times
+cat /dev/random | head -c 512 >> /etc/rnd/random
+openssl rand -rand /etc/rnd/random 200 >> /etc/rnd/random_openssl
+cat /dev/urandom | head -c 100 >> /etc/rnd/random
+openssl rand -rand /etc/rnd/random 200 >> /etc/rnd/random_openssl
+cat /dev/urandom | head -c 100 >> /etc/rnd/random
+openssl rand -rand /etc/rnd/random 200 >> /etc/rnd/random_openssl
+
+if [[ "$DOCKER" == "" ]]; then
+	echo "Please type in your Docker container's name. For help, type -h"
+fi
+
+docker stop "$DOCKER" >> /dev/null
+docker cp /etc/rnd/random_openssl "$DOCKER":/dev/random
+docker start "$DOCKER" >> /dev/null
+
+if [[ "$APP" = "ssh" || "$APP" = "SSH" ]]; then
+	echo "Changing SSH keys, you may get a known hosts error when connecting next time"
+	docker exec $DOCKER /bin/rm -v /etc/ssh/ssh_host_*
+	docker exec $DOCKER dpkg-reconfigure openssh-server
+	echo "Restarting SSH service, beware"
+	docker exec $DOCKER /etc/init.d/ssh restart
+fi
+
+rm -r /etc/rnd
+echo "" >> /etc/rnd/random.log
---
+++ build.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Script is from InnovativeInventor/pivpn, with some modifications
+
+echo "Running . . ."
+
+# Pulling from docker
+docker pull innovativeinventor/docker-pivpn
+
+# Getting random.sh file
+mkdir -p assets
+curl -L https://bit.ly/2uGNBbW -o assets/random.sh
+
+# Checking ports
+BASE=522
+INCREMENT=1
+port=$BASE
+isfree=$(lsof -i -n -P | grep $port)
+
+# Adding one every time a port is used up
+while [[ -n "$isfree" ]]; do
+  port=$[port+INCREMENT]
+  isfree=$(lsof -i -n -P | grep $port)
+done
+
+# Getting password for container
+echo Docker password:
+read -s password
+
+# Checking if name exists
+
+# Adding docker is free varible and docker is exited varible
+dockerisfree=$(docker ps -q -f name=pivpn$num)
+dockerisexited=$(docker ps -aq -f status=exited -f name=pivpn)
+
+# If statements
+if [[ -n "$dockerisfree" ]]; then
+
+	# Checking if it is exited
+    if [[ -n "$dockerisexited" ]]; then
+
+    	# Asking user if it is okay to delete exited container with the same name
+    	echo "A exited version of docker-pivpn has been detected, do you want to delete it?"
+    	read -r -p "Are you sure? [y/N] " response
+		if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+		then
+
+			# Creating everything, then exiting to prevent errors
+    		docker rm pivpn
+    		docker run --name=pivpn -d -p $port:22 innovativeinventor/docker-pivpn
+			sudo assets/random.sh -d pivpn -a ssh
+			printf "$password\n$password\n"  | docker exec -i pivpn$ passwd root
+			echo "Done! A container with the name pivpn and the pivpn port $port has been created for you. Entropy has been added to the system from this server, and the ssh keys have been regenerated."
+			exit 10
+
+    	elif [[ "$response" =~ ^([nN][oO]|[nN])+$ ]]
+    	then
+    		# Allowing to proceed
+    		echo "Okay, creating a new container with different name"
+
+		else
+    		echo "Error, invalid input"
+		fi
+    fi
+
+    # Figuring out what number suffix to attach to the end
+	DOCKERBASE=1
+	DOCKERINCREMENT=1
+
+	num=$DOCKERBASE
+
+	while [[ -n "$dockerisfree" ]];do
+		num=$[num+DOCKERINCREMENT]
+		dockerisfree=$(docker ps -q -f name=pivpn$num)
+	done
+	echo $num
+
+	# Creating everything, then exiting to prevent errors
+    docker run --name=pivpn$num -d -p $port:22 innovativeinventor/docker-pivpn
+	sudo assets/random.sh -d pivpn$num -a ssh
+	printf "$password\n$password\n"  | docker exec -i pivpn$num passwd root
+  echo "Done! A container with the name pivpn$num and the pivpn port $port has been created for you. Entropy has been added to the system from this server, and the shh keys have been regenerated."
+	exit 10
+fi
+
+# Installing
+docker run --name=pivpn -d -p $port:22 innovativeinventor/docker-pivpn
+sudo assets/random.sh -d pivpn -a ssh
+printf "$password\n$password\n"  | docker exec -i pivpn passwd root
+echo "Done! A container with the name pivpn and the pivpn port $port has been created for you. Entropy has been added to the system from this server, and the ssh keys have been regenerated."
