843ed3930b257557dc51d2ba012329809b515f3c
InnovativeInventor <InnovativeInventor@gmail.com>
2017-07-19 22:09:01 -0400

    Some changes

--- assets/setup.sh
+++ assets/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Options
 while getopts d:p:s option
 do
 	case "${option}"
@@ -8,11 +10,15 @@ do
 	esac
 done
 
-curl -L https://install.pivpn.io -o /usr/bin/pivpn
-echo "rm /usr/bin/pivpn" >> /usr/bin/pivpn
-chmod +x pivpn
+# Getting latest copy of PiVPN script
+docker exec $DOCKER curl -L https://install.pivpn.io -o /usr/bin/pivpn
+docker exec $DOCKER echo "rm /usr/bin/pivpn" >> /usr/bin/pivpn
+docker exec $DOCKER chmod +x pivpn
 
-mv assets/motd.tail /etc/motd.tail
+# Adding MOTD
+docker cp assets/motd.tail $DOCKER:/etc/motd.tail
 
+# SSHing into docker container
+ssh-keygen -R [127.0.0.1]:$PORT
 echo "Logging into $DOCKER, type in your $DOCKER password"
 ssh root@127.0.0.1 -p $PORT
